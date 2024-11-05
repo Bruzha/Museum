3129a00001756f2935ce73520db65bf1c9991d46
Bruzha <dashabry15@gmail.com>
2024-11-05 14:02:27 +0300

    разрешения для фото

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,39 @@
 const path = require('path')
+const phtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: {
         filename: path.resolve(__dirname,'src/index.js')
     },
     output:{
         path: path.resolve(__dirname,'dist'),
-        filename: 'index.js'
+        filename: 'index.js',
+        assetModuleFilename: '[name][ext]'
+    },
+    devServer:{
+        port:9000,
+        compress: true,
+        hot: true,
+        static:{
+            directory: path.join(_dirname, 'dist')
+        }
     },
     module:{
         rules:[
             {
                 test: /\.scss$/,
                 use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
             }
         ]
-    }
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title: 'Museum',
+            filename: 'index.html'
+        })
+    ]
 }
