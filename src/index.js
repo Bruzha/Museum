9e61885287ca540915db0b493d1ca5586e638588
Bruzha <dashabry15@gmail.com>
2024-11-12 17:47:08 +0300

    creating carousels

--- src/index.js
+++ src/index.js
@@ -61,5 +61,92 @@ function Cost() {
   let count3 = document.getElementById('count1').textContent;
   let count4 = document.getElementById('count2').textContent;
   document.getElementById('cost').textContent =
-    'Total €' + (cost * count3 + cost * count4).toString();
+    'Total €' + (20 * count3 + 10 * count4).toString();
 }
+
+let b2 = document.getElementById('b2');
+b2.onclick = function () {
+  second_window = window.open('../dist/booking.html', '_blank');
+};
+
+let i_img = 0;
+let mas_img = new Array(
+  'C:/Users/DEll/Desktop/ПССИП/Практика КОДДОТ/Museum/src/img/1.png',
+  'C:/Users/DEll/Desktop/ПССИП/Практика КОДДОТ/Museum/src/img/2.png',
+  'C:/Users/DEll/Desktop/ПССИП/Практика КОДДОТ/Museum/src/img/3.png',
+  'C:/Users/DEll/Desktop/ПССИП/Практика КОДДОТ/Museum/src/img/4.png',
+  'C:/Users/DEll/Desktop/ПССИП/Практика КОДДОТ/Museum/src/img/5.png'
+);
+
+let first_img = document.getElementById('main_img');
+let radio = document.querySelectorAll('.class1');
+document.getElementById('arrow1').onclick = function () {
+  i_img--;
+  if (i_img < 0) i_img = 4;
+  first_img.src = mas_img[i_img];
+  document.getElementById('count_img').textContent =
+    '0' + (i_img + 1).toString();
+  for (let i = 0; i < 5; i++) {
+    if (i === i_img) radio[i].className = 'radio-look2';
+    else radio[i].className = 'radio-look';
+  }
+};
+document.getElementById('arrow2').onclick = function () {
+  i_img++;
+  if (i_img > 4) i_img = 0;
+  first_img.src = mas_img[i_img];
+  document.getElementById('count_img').textContent =
+    '0' + (i_img + 1).toString();
+  for (let i = 0; i < 5; i++) {
+    if (i === i_img) radio[i].className = 'radio-look2';
+    else radio[i].className = 'radio-look';
+  }
+};
+
+/*let i_video = 0;
+let i_video2 = 0;
+let i_video3 = 0;*/
+let video = document.querySelectorAll('.frame');
+let span = document.querySelectorAll('.class2');
+let video_src = new Array(
+  'https://www.youtube.com/embed/aWmJ5DgyWPI?si=H85tY1qiikp8xfTx',
+  'https://www.youtube.com/embed/Vi5D6FKhRmo?si=WhHSVVS17bNR0Rx9',
+  'https://www.youtube.com/embed/NOhDysLnTvY?si=5t7R4CF4vOVp_k1n',
+  'https://www.youtube.com/embed/VVj-2Jdtl4o?si=2eQCWj_A3kqzbJEu',
+  'https://www.youtube.com/embed/zp1BXPX8jcU?si=Zy4Job7HFMv4NsT5'
+);
+let i_video = new Array(0, 4, 3);
+document.getElementById('arrow21').onclick = function () {
+  i_video[0]--;
+  i_video[1]--;
+  i_video[2]--;
+  if (i_video[0] < 0) i_video[0] = 4;
+  if (i_video[1] < 0) i_video[1] = 4;
+  if (i_video[2] < 0) i_video[2] = 4;
+  for (let i = 0; i < 3; i++) {
+    video[i].src = video_src[i_video[i]];
+  }
+  for (let i = 0; i < 5; i++) {
+    if (i === i_video[0]) span[i].className = 'radio-look4';
+    else span[i].className = 'radio-look3';
+  }
+};
+document.getElementById('arrow22').onclick = function () {
+  i_video[0]++;
+  i_video[1]++;
+  i_video[2]++;
+  if (i_video[0] > 4) i_video[0] = 0;
+  if (i_video[1] > 4) i_video[1] = 0;
+  if (i_video[2] > 4) i_video[2] = 0;
+  for (let i = 0; i < 3; i++) {
+    video[i].src = video_src[i_video[i]];
+  }
+  for (let i = 0; i < 5; i++) {
+    if (i === i_video[0]) span[i].className = 'radio-look4';
+    else span[i].className = 'radio-look3';
+  }
+  for (let i = 0; i < 5; i++) {
+    if (i === i_video[0]) span[i].className = 'radio-look4';
+    else span[i].className = 'radio-look3';
+  }
+};
