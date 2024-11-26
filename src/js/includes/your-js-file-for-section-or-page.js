65e9fe1cc8564859cbe9bc72c5c1abb15e33c279
Bruzha <dashabry15@gmail.com>
2024-11-26 13:04:54 +0300

    modal window

--- src/js/includes/your-js-file-for-section-or-page.js
+++ src/js/includes/your-js-file-for-section-or-page.js
@@ -3,6 +3,7 @@
 
 // }
 
-// class Welcome{
+class Welcome{
+    
 
-// }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import Swiper from "swiper";
-// import { heroOptions } from "./includes/your-js-file-for-section-or-page";
+import { heroOptions } from "./includes/your-js-file-for-section-or-page";
 
 if (document.location.pathname === "/" || document.location.pathname === "/index.html") {
   //Hero init your class from your-js-file-for-section-or-pag
