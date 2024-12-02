1e65d56f90ad83da22f0b641cb66e95db1fef9d7
Bruzha <dashabry15@gmail.com>
2024-12-02 17:27:32 +0300

    create registration-page

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,21 @@
 import Swiper from "swiper";
-import { heroOptions } from "./includes/your-js-file-for-section-or-page";
-
+// import { heroOptions } from "./includes/your-js-file-for-section-or-page";
+import Classes from './includes/your-js-file-for-section-or-page'
 if (document.location.pathname === "/" || document.location.pathname === "/index.html") {
   //Hero init your class from your-js-file-for-section-or-pag
   // new YourClass()
 
 }
 
-// if (document.location.pathname === "/your-page.html") {
-//   //code
-// }
-
+if (document.location.pathname === "/your-page.html") {
+  //code
+}
+document.addEventListener('DOMContentLoaded', () => {
+  const welcome = new Classes.Welcome();
+  const header = new Classes.Header();
+  const tickets = new Classes.Tickets();
+  const booking = new Classes.Booking();
+  const video = new Classes.Video();
+  const explore = new Classes.Explore();
+  const registration = new Classes.Registration();
+});
