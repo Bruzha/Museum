cf227ce299b490029800a8f7b12640f89da42e77
Bruzha <dashabry15@gmail.com>
2024-12-16 13:46:03 +0300

    update

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,20 +4,20 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyBSAaMmNbEutjwNYBgFrfVAvyyhIJvCMAE",
-  // authDomain: "museum-4007c.firebaseapp.com",
-  // projectId: "museum-4007c",
-  // storageBucket: "museum-4007c.appspot.com",
-  // messagingSenderId: "443847601599",
-  // appId: "1:443847601599:web:8d907089bf34bd3698050a",
-  // databaseURL: 'https://museum-4007c-default-rtdb.firebaseio.com/',
-  apiKey: import.meta.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  databaseURL: process.env.databaseURL
+  apiKey: "AIzaSyBSAaMmNbEutjwNYBgFrfVAvyyhIJvCMAE",
+  authDomain: "museum-4007c.firebaseapp.com",
+  projectId: "museum-4007c",
+  storageBucket: "museum-4007c.appspot.com",
+  messagingSenderId: "443847601599",
+  appId: "1:443847601599:web:8d907089bf34bd3698050a",
+  databaseURL: 'https://museum-4007c-default-rtdb.firebaseio.com/',
+  // apiKey: import.meta.env.apiKey,
+  // authDomain: process.env.authDomain,
+  // projectId: process.env.projectId,
+  // storageBucket: process.env.storageBucket,
+  // messagingSenderId: process.env.messagingSenderId,
+  // appId: process.env.appId,
+  // databaseURL: process.env.databaseURL
 };
 
 const app = initializeApp(firebaseConfig);
